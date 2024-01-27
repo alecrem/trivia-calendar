19c2a7f27aef94fcc80d0d3ba22798ec5c842c43
Alejandro Cremades <ale@alecrem.com>
2024-01-28 00:33:51 +0900

    API should find the date sent, regardless of the server's time zone

--- src/app/api/getdate/[year]/[month]/[day]/route.ts
+++ src/app/api/getdate/[year]/[month]/[day]/route.ts
@@ -2,12 +2,18 @@ import { NextResponse, NextRequest } from 'next/server'
 import { getDate } from '@/app/api/db'
 
 type Params = { year: string; month: string; day: string }
+const twoDigitFormatter = new Intl.NumberFormat('en-US', {
+  minimumIntegerDigits: 2,
+})
 
 export const GET = async (
   request: NextRequest,
   { params }: { params: Params }
 ) => {
-  const requestedDate = new Date(`${params.year}-${params.month}-${params.day}`)
+  const month = twoDigitFormatter.format(+params.month)
+  const day = twoDigitFormatter.format(+params.day)
+  const dateString = `${params.year}-${month}-${day}T00:00:00.000Z`
+  const requestedDate = new Date(dateString)
   const data = await getDate(requestedDate)
   if (data.length < 1)
     return NextResponse.json(
